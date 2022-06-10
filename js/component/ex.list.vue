af36b92317abcc9b484126b7f7be5b1c179b90ad
Rake Yang <fenglaijun@gmail.com>
2022-06-10 15:08:58 +0800

    增加列表内容

---
+++ js/component/ex.list.vue
@@ -0,0 +1,32 @@
+<template>
+    <el-row>
+        <el-col v-for="item in list">
+            {{ item.index }} {{ item.value }}
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            list: [],
+            contentHeight: parent.vm.contentHeight - 40
+        };
+    },
+    mounted: function () {
+        const obj = this
+        for (let i = 0; i < 200; i++) {
+            this.list.push({ index: i, value: Math.random() });
+        }
+        window.onresize = () => {
+            return (() => {
+                window.screenHeight = document.body.clientHeight - 59;
+                obj.screenHeight = window.screenHeight;
+            })()
+        };
+    },
+    methods: {
+    }
+};
+</script>
---
+++ js/component/ex.table.vue
@@ -0,0 +1,40 @@
+<template>
+    <el-row>
+        <el-col>
+            <el-table :data="list" size="mini" :max-height="contentHeight" border header-row-style="font-width:bold;"
+                header-cell-style="background-color:#e0e0e0;">
+                <el-table-column prop="index" label="序号"></el-table-column>
+                <el-table-column prop="value" label="内容"></el-table-column>
+            </el-table>
+        </el-col>
+        <el-col style="margin-top:8px;">
+            <el-pagination background layout="prev, pager, next" size="mini" :total="list.length">
+            </el-pagination>
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            list: [],
+            contentHeight: parent.vm.contentHeight - 40
+        };
+    },
+    mounted: function () {
+        const obj = this
+        for (let i = 0; i < 200; i++) {
+            this.list.push({ index: i, value: Math.random() });
+        }
+        window.onresize = () => {
+            return (() => {
+                window.screenHeight = document.body.clientHeight - 59;
+                obj.screenHeight = window.screenHeight;
+            })()
+        };
+    },
+    methods: {
+    }
+};
+</script>
