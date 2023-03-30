89f8e4b0ff7fe34a79b7e4bbe8b94833b9ccee8b
Rake Yang <fenglaijun@gmail.com>
2023-03-30 20:12:16 +0800

    Vue封装&局部mixin

--- js/plugin.js
+++ js/plugin.js
@@ -1,11 +1,40 @@
-Vue.mixin({
-    data: function () {
+const BaseMixin = {
+    data() {
         return {
+            mixin: {
+                version: '0.1.0',
+                loading: false,
+                prepared: false
+            }
         };
     },
-    mounted: function () {
-        // if (this.loadData != undefined) {
-        //     this.loadData();
-        // }
+    mounted() {
+        this.mixin.loading = true;
+        this.preparation();
+    },
+    watch: {
+        "mixin.prepared": function (newValue, oldValue) {
+            if (newValue && newValue != oldValue) {
+                if (this.loaddata) {
+                    this.loaddata(1);
+                }
+                this.mixin.loading = false;
+            }
+        },
+    },
+    methods: {
+        preparation() {
+            this.mixin.prepared = true;
+        }
+    },
+};
+
+const VueComponment = {
+    name: "BaseVue",
+    mixins: [BaseMixin],
+    data() {
+        return { based: true };
     }
-});
+};
+
+const Vue2 = Vue.extend(VueComponment);
